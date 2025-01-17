82d551894d944badadbd1b28d6769173e5bf55a7
MrAphell <polonkai.oliver@gmail.com>
2025-01-17 19:46:41 +0100

    Create api.php

---
+++ Gamer_Webshop/routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::name('api.products.')->prefix('products')->group(function () {
+    Route::get('/', [ProductController::class, 'index'])->name('index');
+    Route::post('/', [ProductController::class, 'store'])->name('store');
+    Route::get('/{product}', [ProductController::class, 'show'])->name('show');
+    Route::put('/{product}', [ProductController::class, 'update'])->name('update');
+    Route::delete('/{product}', [ProductController::class, 'destroy'])->name('destroy');
+});
+
+Route::name('api.categories.')->prefix('categories')->group(function () {
+    Route::get('/', [CategoryController::class, 'index'])->name('index');
+    Route::post('/', [CategoryController::class, 'store'])->name('store');
+    Route::get('/{category}', [CategoryController::class, 'show'])->name('show');
+    Route::put('/{category}', [CategoryController::class, 'update'])->name('update');
+    Route::delete('/{category}', [CategoryController::class, 'destroy'])->name('destroy');
+});
+
+Route::name('api.orders.')->prefix('orders')->group(function () {
+    Route::get('/', [OrderController::class, 'index'])->name('index');
+    Route::post('/', [OrderController::class, 'store'])->name('store');
+    Route::get('/{order}', [OrderController::class, 'show'])->name('show');
+    Route::delete('/{order}', [OrderController::class, 'destroy'])->name('destroy');
+});
