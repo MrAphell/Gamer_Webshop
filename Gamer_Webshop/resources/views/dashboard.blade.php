829620613bf86898df1c6e4036f97ee31e737e32
MrAphell <polonkai.oliver@gmail.com>
2025-01-17 19:40:13 +0100

    Dashboard succes form update

--- Gamer_Webshop/resources/views/dashboard.blade.php
+++ Gamer_Webshop/resources/views/dashboard.blade.php
@@ -1,15 +1,15 @@
 @extends('app')
 
 @section('content')
-    <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
+    <div class="container d-flex justify-content-center mt-5" style="height: 100vh;">
         <div class="col-md-6">
             <div class="card shadow-lg">
-                <div class="card-header bg-primary text-white text-center">
-                    <h3>Üdvözöllek {{ Auth::user()->name }}!</h3>
+                <div class="card-header text-white text-center">
+                    <h3>Üdvözöllek {{ Auth::user()->name }}</h3>
                 </div>
                 <div class="card-body">
                     <div class="text-center">
-                        <h4 class="text-gray-900">Sikeresen bejelentkeztél a rendszerbe!</h4>
+                        <h4 class="text-gray-900">Sikeres Bejelentkezés!</h4>
                     </div>
                 </div>
             </div>
